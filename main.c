4bf38247080f83469bf554f0d8cc321f78fbc816
Ubuntu <ubuntu@ip-172-31-29-110.ec2.internal>
2022-02-28 06:11:38 +0000

    added C source code

---
+++ main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void biggest2(void);
+void factorial(void);
+void reverse(void);
+void palindrome(void);
+void biggest3(void);
+void sum(void);
+void fibanaci(void);
+void sortnum(void);
+int main() {
+
+         biggest2();
+         factorial();
+        reverse();
+	palindrome();
+	biggest3();
+	sum();
+	fibanaci();
+	sortnum();
+	return 0;
+}
